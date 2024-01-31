7635c1e6bdff7a800e4aa813e5b80907415b596d
Olushola Joshua <olusholajoshua7@gmail.com>
2024-01-31 10:34:59 -0600

    initial commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="flex justify-center items-center h-full w-full">
+            {children}
+        </div>
+    )
+}
+
+export default AuthLayout
